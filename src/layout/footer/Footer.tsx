4cc9c556e7391af3e9be2e67ff2a0c052648b31d
Anton-Osip <osipchika@gmail.com>
2024-05-24 17:26:31 +0300

    feat: Designed the structure of the footer section

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+import { FlexWrapper } from '../../components/FlexWrapper'
+import { Icon } from '../../components/icon/Icon'
+
+export function Footer() {
+	return (
+		<StyledFooter>
+			<FlexWrapper direction='column' align='center'>
+				<Name>Svetlana</Name>
+				<SocialList>
+					<SocialItem>
+						<SocialLink>
+							<Icon
+								idIcon='instagramIcon'
+								width='21px'
+								height='21px'
+								viewBox='0 0 21px 21px'
+							/>
+						</SocialLink>
+					</SocialItem>
+					<SocialItem>
+						<SocialLink>
+							<Icon
+								idIcon='telegramIcon'
+								width='21px'
+								height='21px'
+								viewBox='0 0 21px 21px'
+							/>
+						</SocialLink>
+					</SocialItem>
+					<SocialItem>
+						<SocialLink>
+							<Icon
+								idIcon='vkIcon'
+								width='21px'
+								height='21px'
+								viewBox='0 0 21px 21px'
+							/>
+						</SocialLink>
+					</SocialItem>
+					<SocialItem>
+						<SocialLink>
+							<Icon
+								idIcon='linkedinIcon'
+								width='21px'
+								height='21px'
+								viewBox='0 0 21px 21px'
+							/>
+						</SocialLink>
+					</SocialItem>
+				</SocialList>
+				<Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</Copyright>
+			</FlexWrapper>
+		</StyledFooter>
+	)
+}
+
+const StyledFooter = styled.footer`
+	min-height: 30vh;
+	background-color: #ffcf8b;
+`
+const Name = styled.span``
+const SocialList = styled.ul`
+	display: flex;
+	gap: 30px;
+`
+const SocialItem = styled.li``
+const SocialLink = styled.a``
+const Copyright = styled.small``
