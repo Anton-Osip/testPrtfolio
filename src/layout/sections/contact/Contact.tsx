d299f3e6413403e037fd31141a5a2640f5610352
Anton-Osip <osipchika@gmail.com>
2024-05-30 20:10:44 +0300

    feat: add styles for Contacts section

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,31 +1,59 @@
 import styled from 'styled-components'
 import { Button } from '../../../components/Button'
+import { Container } from '../../../components/Container'
 import { SectionTitle } from '../../../components/SectionTitle'
+import { theme } from '../../../styles/Theme'
 
 export function Contact() {
 	return (
 		<StyledContacts>
-			<SectionTitle>Contact</SectionTitle>
-			<StyledForm>
-				<Field placeholder='name' />
-				<Field placeholder='subject' />
-				<Field placeholder='Message' as={'textarea'} />
-				<Button type='submit'>Send message</Button>
-			</StyledForm>
+			<Container>
+				<SectionTitle>Contact</SectionTitle>
+				<StyledForm>
+					<Field placeholder='name' />
+					<Field placeholder='subject' />
+					<Field placeholder='Message' as={'textarea'} />
+					<Button type='submit'>Send message</Button>
+				</StyledForm>
+			</Container>
 		</StyledContacts>
 	)
 }
 
-const StyledContacts = styled.section`
-	min-height: 50vh;
-	background-color: #ffebab;
-`
+const StyledContacts = styled.section``
+
 const StyledForm = styled.form`
-	max-width: 500px;
+	max-width: 540px;
 	width: 100%;
 	display: flex;
 	flex-direction: column;
-	gap: 10px;
+	align-items: center;
+	gap: 16px;
 	margin: 0 auto;
+
+	textarea {
+		resize: none;
+		height: 155px;
+	}
 `
-const Field = styled.input``
+const Field = styled.input`
+	width: 100%;
+	border: 1px solid ${theme.colors.borderColor};
+	background-color: ${theme.colors.secondaryBg};
+	padding: 7px 15px;
+	color: ${theme.colors.font};
+	font-family: Poppins;
+	font-size: 12px;
+	font-weight: 400;
+	line-height: 18px;
+	letter-spacing: 5%;
+
+	&::placeholder {
+		color: ${theme.colors.placeholderColor};
+		text-transform: capitalize;
+	}
+
+	&:focus-visible {
+		outline: 1px solid ${theme.colors.borderColor};
+	}
+`
