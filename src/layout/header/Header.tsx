4a48c7c250739966090277cabc9cf2b7e7c33130
Anton-Osip <osipchika@gmail.com>
2024-06-04 20:50:23 +0300

    feat: made adaptiv for header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,6 +3,7 @@ import { Container } from '../../components/Container'
 import { FlexWrapper } from '../../components/FlexWrapper'
 import { Logo } from '../../components/logo/Logo'
 import { HeaderMenu } from './headerMenu/HeaderMenu'
+import { MobileMenu } from './mobileMenu/MobileMenu'
 const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contact']
 export function Header() {
 	return (
@@ -11,6 +12,7 @@ export function Header() {
 				<FlexWrapper justify='space-between' align='center'>
 					<Logo />
 					<HeaderMenu menuItems={items} />
+					<MobileMenu menuItems={items} />
 				</FlexWrapper>
 			</Container>
 		</StyledHeader>
